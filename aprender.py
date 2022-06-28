88d810300a3384e042686f64f9ecaf3da9196a2d
MauricioMumo21 <84625773+MauricioMumo21@users.noreply.github.com>
2022-06-27 22:35:11 -0300

    Add files via upload

---
+++ aprender.py
@@ -0,0 +1,10 @@
+print("hola mundo")
+print("hola aprender")
+
+import numpy as np
+import pandas as pd
+import json
+import streamlit as st
+import unicodedata
+
+uribe_df=pd.read_excel("C:/Users/Mauricio/Desktop/SEMIF.xlsx")
